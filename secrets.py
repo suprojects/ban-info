09723dae076f0e0614b13dec459840b641d77d8c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2020-12-28 12:07:28 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,14 +1,7 @@
 import os
 
-SPAMWATCH_TOKEN = "wedfrtgyh"
-ANTISPAMINC_TOKEN = "edrftgyh"
-BOT_TOKEN = "edfrgbh"
-SUDO_USERS = []
-LOG_ID = 123456
-
-if os.environ.get("ENV"):
-  SPAMWATCH_TOKEN = os.environ.get('SPAMWATCH')
-  ANTISPAMINC_TOKEN = os.environ.get('ANTISPAMINC')
-  BOT_TOKEN = os.environ.get('BOT_TOKEN')
-  SUDO_USERS = os.environ.get('SUDO_USERS').split()
-  LOG_ID = os.environ.get('LOG_ID')
+SPAMWATCH_TOKEN = os.environ.get('SPAMWATCH')
+ANTISPAMINC_TOKEN = os.environ.get('ANTISPAMINC')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+SUDO_USERS = os.environ.get('SUDO_USERS').split()
+LOG_ID = os.environ.get('LOG_ID')
