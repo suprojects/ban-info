4c6f73d8d905ef40de6ba6b85adcbf88d414c77a
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2020-12-31 10:29:41 +0530

    add change antispaminc_token to nospamplus_token

--- secrets.py
+++ secrets.py
@@ -1,11 +1,5 @@
 import os
 
-os.environ['SPAMWATCH'] = 'fUG438ge8iVPrf7SfwFxCPNnY2Q0P2IiaVOhviePNsIo31m8nQwr~gzku5x_7gV0'
-os.environ['NOSPAMPLUS'] = 'UQxkLhHV_GEH7JR23SwOhA'
-os.environ['BOT_TOKEN'] = '1459172207:AAExAyKQXHBWmFa4Zt9RYlLHwhpSygF9ZYM'
-os.environ['SUDO_USERS'] = '1178472788'
-os.environ['LOG_ID'] = '-1001146754909'
-
 SPAMWATCH_TOKEN = os.environ.get("SPAMWATCH")
 NOSPAMPLUS_TOKEN = os.environ.get("NOSPAMPLUS")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
