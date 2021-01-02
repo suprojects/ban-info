2a9225f2ae5cdb800a435264761059d7833f84dc
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-01-02 08:33:50 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -7,4 +7,4 @@ SPAMWATCH_TOKEN = os.environ.get("SPAMWATCH")
 NOSPAMPLUS_TOKEN = os.environ.get("NOSPAMPLUS")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 SUDO_USERS = int(os.environ.get("SUDO_USERS"))
-LOG_CHAT = os.environ.get("LOG_ID")s
+LOG_CHAT = os.environ.get("LOG_ID")
