b1f347a25fffad9e113874d2705a8bbbc1b17f3c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-01-02 08:48:12 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,15 @@
 import os
 
 if os.path.exists('tokens.py'):
-    import tokens
+
+    from tokens import *
+
+    os.environ['SPAMWATCH'] = SPAMWATCH
+    os.environ['NOSPAMPLUS'] = NOSPAMPLUS
+    os.environ['BOT_TOKEN'] = BOT_TOKEN
+    os.environ['SUDO_USERS'] = SUDO_USERS
+    os.environ['LOG_ID'] = LOG_ID
+
 
 SPAMWATCH_TOKEN = os.environ.get("SPAMWATCH")
 NOSPAMPLUS_TOKEN = os.environ.get("NOSPAMPLUS")
