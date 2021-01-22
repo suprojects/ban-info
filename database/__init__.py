ade299fbaf7c44df1f8a639e134ceb4c80afacdd
Roj <rojserbest@gmail.com>
2021-01-22 12:44:32 +0300

    wwwwwwwwwwwwwwwww

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-URI = "mongodb+srv://alpha:LjvpahvVijuVf4KP@cluster0.wia0s.mongodb.net/baninforobot"
+URI = "mongodb+srv://alpha:LjvpahvVijuVf4KP@cluster0.wia0s.mongodb.net/baninfobot"
 client = MongoClient(URI)
-db = client["baninforobot"]
+db = client["baninfobot"]
