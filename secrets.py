428a11aab9a78e962f0d934cad4d7566b69df25b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-01-23 09:56:56 +0530

    update secrets for DB URI

--- secrets.py
+++ secrets.py
@@ -11,6 +11,7 @@ if os.path.exists("tokens.py"):
     os.environ["SUDO_USERS"] = SUDO_USERS
     os.environ["LOG_ID"] = LOG_ID
     os.environ["OWLANTISPAM"] = OWLANTISPAM
+    os.environ["URI"] = URI
 
 
 SPAMWATCH_TOKEN = os.environ.get("SPAMWATCH")
@@ -20,3 +21,4 @@ SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS").split()))
 LOG_CHAT = os.environ.get("LOG_ID")
 OWLANTISPAM_TOKEN = os.environ.get("OWLANTISPAM")
 SUDO_ONLY = Filters.user(SUDO_USERS)
+URI = os.environ.get("URI")
