3d979fc7c7b4f987dc092462bcb134e06417ec28
Roj Serbest <rojserbest@gmail.com>
2021-01-31 21:45:34 +0300

    You don't need if user.name else None because it's naturally None if there's none

--- database/botusers.py
+++ database/botusers.py
@@ -1,19 +1,18 @@
 from database import db
+from database import tgusers
 
-botusers = db['botusers']
+botusers = db["botusers"]
+
 
 def new_user(from_user):
-
-    from database import tgusers
     tgusers.update_user(from_user)
-
     botusers.update_one(
         {"id": from_user.id},
         {
             "$set": {
-                "username": from_user.username if from_user.username else None,
+                "username": from_user.username,
                 "firstname": from_user.first_name,
-                "lastname": from_user.last_name if from_user.last_name else None,
+                "lastname": from_user.last_name,
             }
         },
         upsert=True,
@@ -22,6 +21,7 @@ def new_user(from_user):
 
 def remove_user(userid):
     botusers.delete_one({"id": userid})
+
 
 def bot_users():
     return list(botusers.find())
