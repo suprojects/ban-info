7c82f03e9c7565acd0f4b79bee841c136b48b9d0
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-02-01 14:38:48 +0530

    reworked secrets

--- secrets.py
+++ secrets.py
@@ -1,16 +1,18 @@
-
 from telegram.ext import Filters
 
 try:
     from config import *
+
 except ImportError:
-    from os import environ
-    SPAMWATCH_TOKEN = environ.get("SPAMWATCH")
-    NOSPAMPLUS_TOKEN = environ.get("NOSPAMPLUS")
-    BOT_TOKEN = environ.get("BOT_TOKEN")
-    SUDO_USERS = list(map(int, environ.get("SUDO_USERS").split()))
-    LOG_CHAT = environ.get("LOG_ID")
-    OWLANTISPAM_TOKEN = environ.get("OWLANTISPAM")
-    URI = environ.get("URI")
+    pass
+
+from os import environ
+SPAMWATCH_TOKEN = environ.get("SPAMWATCH")
+NOSPAMPLUS_TOKEN = environ.get("NOSPAMPLUS")
+BOT_TOKEN = environ.get("BOT_TOKEN")
+SUDO_USERS = list(map(int, environ.get("SUDO_USERS").split()))
+LOG_CHAT = environ.get("LOG_ID")
+OWLANTISPAM_TOKEN = environ.get("OWLANTISPAM")
+URI = environ.get("URI")
 
 SUDO_ONLY = Filters.user(SUDO_USERS)
