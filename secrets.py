06c743464283c5b51e45a2b87710e2f848c2d3c8
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-02-12 21:42:56 +0530

    same name for vars and config vars

--- secrets.py
+++ secrets.py
@@ -7,12 +7,12 @@ except ImportError:
     pass
 
 from os import environ
-SPAMWATCH_TOKEN = environ.get("SPAMWATCH")
-NOSPAMPLUS_TOKEN = environ.get("NOSPAMPLUS")
+SPAMWATCH_TOKEN = environ.get("SPAMWATCH_TOKEN")
+NOSPAMPLUS_TOKEN = environ.get("NOSPAMPLUS_TOKEN")
 BOT_TOKEN = environ.get("BOT_TOKEN")
 SUDO_USERS = list(map(int, environ.get("SUDO_USERS").split()))
 LOG_CHAT = environ.get("LOG_CHAT")
-OWLANTISPAM_TOKEN = environ.get("OWLANTISPAM")
+OWLANTISPAM_TOKEN = environ.get("OWLANTISPAM_TOKEN")
 URI = environ.get("URI")
 
 SUDO_ONLY = Filters.user(SUDO_USERS)
